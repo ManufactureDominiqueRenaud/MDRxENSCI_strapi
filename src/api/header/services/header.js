1e054f0bb8bcaa1e1c8591ba619776b309eed1ab
Frédérick FLORKOWSKI <perso.dsgjohnson@outlook.fr>
2025-03-02 16:35:25 +0100

    added the conent type header and footer

--- src/api/header/services/header.js
+++ src/api/header/services/header.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * blog service
+ * header service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::blog.blog');
+module.exports = createCoreService('api::header.header');
--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * blog controller
+ * page controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::blog.blog');
+module.exports = createCoreController('api::page.page');
